fcec8460b1f44960afcf0e5ad4b344294feb721a
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-10-16 22:23:33 +0000

    debugging

--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -3,41 +3,25 @@
   import Card from "./Card.svelte";
 
   export let selectedCard: ComponentProps<Card> | boolean;
+
+  $: checked = Boolean(selectedCard);
 </script>
 
-<div class="drawer lg:drawer-open">
-  {#if selectedCard}
-    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" checked/>
-  {:else}
-    <input id="my-drawer-2" type="checkbox" class="drawer-toggle"/>
-  {/if}
-  <div class="drawer-content flex flex-col items-center justify-center">
-    <!-- Page content here -->
-    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">
-      Open drawer
-    </label>
-  </div>
-  <div class="drawer-side">
-    <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
-    <!-- -- >
-    <ul class="menu bg-base-200 text-base-content min-h-full w-30 p-4">
-      <!-- Sidebar content here -- >
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
-    </ul>
-    <!-- -->
 
-    {#if selectedCard}
+<div on:blur={(_) => { selectCard = false; }} class="drawer">
+  <input id="editor-drawer" type="checkbox" class="drawer-toggle" bind:checked/>
+
+  <div class="drawer-side">
+    <label for="editor-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+    
     <section>
       <!-- The Card area -->
       <Card 
         {...selectedCard}
         />
     </section>
-    {/if}
   </div>
 
-  
 
   <section>
     <!-- The "commit" area -->
