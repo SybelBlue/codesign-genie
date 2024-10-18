cb095e769c0742c8bbc6c2fe645d5db33ecb6cda
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-10-18 18:57:43 +0000

    forgot to add the actual changes haha

--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -3,27 +3,50 @@
   import Card from "./Card.svelte";
 
   export let selectedCard: ComponentProps<Card> | undefined;
-
-  $: checked = Boolean(selectedCard);
 </script>
 
 {#if selectedCard}
-  <div on:blur={(_) => { selectedCard = undefined; }} class="drawer">
-    <input id="editor-drawer" type="checkbox" class="drawer-toggle" bind:checked/>
+<div class="absolute fixed left-0 w-1/2 h-screen">
+  <div class="bg-base-100 h-screen grid grid-cols-1">
+    <!-- "X" button in top right -->
+    <button
+      class="btn btn-circle btn-outline"
+      on:click={(_) => {
+        selectedCard = undefined;
+      }}
+      >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M6 18L18 6M6 6l12 12" />
+      </svg>
+    </button>
+    <!-- -->
 
-    <div class="drawer-side w-1/2 bg-slate-100">
-      <label for="editor-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
-
-      <!-- The Card area -->
-      <div class="w-full place-content-center">
-          <Card
-            {...selectedCard}
-            />
-      </div>
-      <!-- The "commit" area -->
-      <input type="text" name="commitMessage" id="commitMessageInput">
-      <input type="submit" value="commit" id="commitSubmitBtn">
+    <!-- The Card area -->
+    <div class="mx-auto w-4/5">
+      <Card
+        {...selectedCard}
+        />
     </div>
+    <!-- -->
+
+    <!-- The "commit" area -->
+    <div class="flex justify-center w-4/5 mx-auto mb-4">
+      <input class="input input-bordered mr-2 w-3/4"
+        type="text" name="commitMessage" id="commitMessageInput">
+      <input class="btn btn-outline w=1/4"
+        type="submit" value="commit" id="commitSubmitBtn">
+    </div>
+    <!-- -->
   </div>
+</div>
 {/if}
 
