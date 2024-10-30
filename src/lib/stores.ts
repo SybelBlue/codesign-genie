3951949505bcf01e5cc8eca463a3c90567dcd1de
Logan Caraco <lbc.dna@gmail.com>
2024-10-30 12:02:18 -0700

    docs?

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -3,7 +3,7 @@ import { toStore, writable } from 'svelte/store';
 
 export const highlightedClass = writable<string | undefined>();
 export const availableClasses = writable<string[]>([]);
-/** a store that is only writable if in a development, else false */
+/** a store that is only writable if in development, else false */
 export const debug = (() => {
   let _debug = dev;
   return toStore(
