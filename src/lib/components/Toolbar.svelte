49988a0b169a005458a158092ebfdf01430af0a1
Logan Caraco <lbc.dna@gmail.com>
2024-11-13 22:16:18 -0800

    suggested style change

--- src/lib/components/Toolbar.svelte
+++ src/lib/components/Toolbar.svelte
@@ -1,12 +1,14 @@
-<script>
+<script lang="ts">
   import ThemeChanger from './ThemeChange.svelte';
   import DeckChanger from './DeckChanger.svelte';
   import { deckNames } from '$lib/stores';
 </script>
 
-<header class="bg-primary text-primary-content shadow-md">
+<header class="shadow-md rounded-b-3xl mb-2">
   <div class="container mx-auto px-4 py-3 flex items-center justify-between">
-    <h1 class="text-xl font-bold">Cara: Your Codesign Genie</h1>
+    <h1 class="text-lg font-mono italic text-accent decoration-primary hover:underline">
+      {"{ cara }"}
+    </h1>
     <nav class="flex items-center gap-4">
       <a href="/" class="px-2 py-1 hover:underline">Sort</a>
       <DeckChanger decks={$deckNames} />
