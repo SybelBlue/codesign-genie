5b4308055960f9b4b34faa7b5d4885a58376ee10
go-bears <melissa.fabros@gmail.com>
2024-11-13 16:10:47 -0800

    timeline panel stub, this works

---
+++ src/lib/components/TimelinePanel.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { slide } from 'svelte/transition';
+    import Timeline from './Timeline.svelte';
+    
+    export let show: boolean;
+  </script>
+  
+  {#if show}
+    <div 
+      class="fixed left-0 right-0 top-16 bg-base-200 shadow-lg p-4 overflow-y-auto z-50"
+      transition:slide={{duration: 300, axis: 'y'}}
+    >
+      <Timeline />
+    </div>
+  {/if}
+
+  
