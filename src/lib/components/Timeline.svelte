ec25fedfc5d874750162c9875470018259697ab2
m.fab <go-bears@users.noreply.github.com>
2024-11-14 14:53:16 -0800

    Update src/lib/components/Timeline.svelte
    remove string markers
    
    Co-authored-by: Logan Caraco <lbc.dna@gmail.com>

--- src/lib/components/Timeline.svelte
+++ src/lib/components/Timeline.svelte
@@ -7,7 +7,9 @@
       { id: 5, text: "Apple Watch", date: "2015" }
     ];
   
-    const checkmarkIcon = `<svg
+  </script>
+  {#snippet checkmarkIcon()}
+    <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 20 20"
       fill="currentColor"
@@ -16,9 +18,9 @@
         fill-rule="evenodd"
         d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
         clip-rule="evenodd" />
-    </svg>`;
-  </script>
-  
+    </svg>
+  {/snippet}
+  <!-- use {@render checkmarkIcon()} instead of {@html checkmarkIcon} -->
   <ul class="timeline timeline-vertical">
     {#each timelineItems as item, index}
       <li>
