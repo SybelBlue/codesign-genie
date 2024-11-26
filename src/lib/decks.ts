c9c498659416340e2c3e8ae198c30c82f4e3d208
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-11-26 21:37:29 +0000

    avoid collisions for ids

--- src/lib/decks.ts
+++ src/lib/decks.ts
@@ -6,7 +6,20 @@ import hospitalJson from '$lib/crc-decks/hospital.json';
 
 export const withId: <T extends object>(o: T) => Keyed<T> = (function () {
   let nextId = 0;
-  return (o) => ({ id: nextId++, ...o });
+  let skipIds = new Set();
+  return (o) => {
+    if ("id" in o) {
+      let id = Number(o.id);
+      if (id != 0 && !id) {
+        throw TypeError(`Encountered non-numeric id!: ${o.id} in ${o}`);
+      }
+      skipIds.add(id);
+    }
+    while (skipIds.has(nextId)) {
+      skipIds.delete(nextId++);
+    }
+    return { id: nextId++, ...o };
+  };
 })();
 
 export const deckWithIds = (deck: DeckJson): SimpleDeck => {
