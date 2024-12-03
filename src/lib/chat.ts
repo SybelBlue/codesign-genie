b6fca8fd84f74318cec464f336426625c71c1ab4
go-bears <melissa.fabros@gmail.com>
2024-12-02 23:30:36 -0800

    new file for building chat strings

--- src/lib/chat.ts
+++ src/lib/chat.ts
@@ -1,6 +1,15 @@
 import type { ValidSchema } from '$lib/types';
 
-function buildContentString(description: string, schema: ValidSchema, typedef: string): string {
+function buildContentString(description: string): string {
+    const content = `Given the following description, please generate an object.
+    \`\`\`description
+    ${description}
+    \`\`\`
+    `;
+    return content;
+};
+
+function buildContentSchemaString(description: string, schema: ValidSchema, typedef: string): string {
     const content = `Given the following description and type definition, please generate an object.
     \`\`\`description
     ${description}
@@ -9,8 +18,9 @@ function buildContentString(description: string, schema: ValidSchema, typedef: s
     \`\`\`typedef
     ${typedef}
     \`\`\`
-    `;
+    Please respond with JSON in the following schema: ${schema}`;
+
     return content;
 };
 
-export { buildContentString };
+export { buildContentSchemaString, buildContentString };
