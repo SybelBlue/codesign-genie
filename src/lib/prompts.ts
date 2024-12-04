aecbf56a6173b4719f85a78f4e6d3a118d8daab2
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-12-04 23:03:57 +0000

    fix prompt template

--- src/lib/prompts.ts
+++ src/lib/prompts.ts
@@ -4,14 +4,14 @@ function buildContentSchemaString(description: string, schema: ValidSchema): str
     const SCHEMA = SCHEMAS[schema];
     const TYPEDEF = TYPEDEFS[schema];
     const content = `Given the following description and type definition, please generate an object.
-    \`\`\`description
-    ${description}
-    \`\`\`
-    
-    \`\`\`typedef
-    ${TYPEDEF}
-    \`\`\`
-    Please respond with JSON in the following schema: ${SCHEMA}`;
+\`\`\`description
+${description}
+\`\`\`
+
+\`\`\`typedef
+${TYPEDEF}
+\`\`\`
+Please respond with JSON in the following schema: ${JSON.stringify(SCHEMA)}`;
 
     return content;
 };
