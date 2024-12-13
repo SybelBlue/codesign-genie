7774f556c2098d0a77b4fbd61edeb6288d05a7a7
Logan Caraco <lbc.dna@gmail.com>
2024-12-13 12:01:28 -0800

    raw responses

---
+++ data_col.fish
@@ -0,0 +1,8 @@
+mkdir datacol-ai
+set -l baseUrl 'localhost:5173';
+for deck in 'A1' 'A2' 'B1' 'B2' 'C1' 'C2' 'C3';
+  for ai in 'cohere' 'openai';
+    set -l outfile "datacol-ai/$deck-$ai.json"
+    curl -X POST $baseUrl/api/solve -H "Content-Type: application/json" -d "{\"backend\":\"$ai\",\"deckCode\":\"$deck\"}" | jq .response > $outfile
+  end
+end
