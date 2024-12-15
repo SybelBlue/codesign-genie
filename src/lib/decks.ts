f64928bb59866592fb8c22534f124336488ca910
Logan Caraco <lbc.dna@gmail.com>
2024-12-14 17:02:27 -0800

    added missing agent

--- src/lib/decks.ts
+++ src/lib/decks.ts
@@ -10,15 +10,15 @@ import libAI0Json from '$lib/crc-decks/data-col/libraryAI0.json';
 import libAI1Json from '$lib/crc-decks/data-col/libraryAI1.json';
 import libCE0Json from '$lib/crc-decks/data-col/libraryCE0.json';
 import libCE1Json from '$lib/crc-decks/data-col/libraryCE1.json';
-import godKill0Json from '$lib/crc-decks/data-col/technical-debt-kill-god0.json';
+import techDebtKillGod0Json from '$lib/crc-decks/data-col/technical-debt-kill-god0.json';
 import techDebtKillGod1Json from '$lib/crc-decks/data-col/technical-debt-kill-god1.json';
-import godMake0Json from '$lib/crc-decks/data-col/technical-debt-make-god0.json';
+import techDebtMakeGod0Json from '$lib/crc-decks/data-col/technical-debt-make-god0.json';
 import techDebtMakeGod1Json from '$lib/crc-decks/data-col/technical-debt-make-god1.json';
-import noAI0Json from '$lib/crc-decks/data-col/casiNoAI0.json';
+import casiNoAI0Json from '$lib/crc-decks/data-col/casiNoAI0.json';
 import casiNoAI1Json from '$lib/crc-decks/data-col/casiNoAI1.json';
-import noWeb0Json from '$lib/crc-decks/data-col/casiNoWeb0.json';
+import casiNoWeb0Json from '$lib/crc-decks/data-col/casiNoWeb0.json';
 import casiNoWeb1Json from '$lib/crc-decks/data-col/casiNoWeb1.json';
-import noCash0Json from '$lib/crc-decks/data-col/casiNoCash0.json';
+import casiNoCash0Json from '$lib/crc-decks/data-col/casiNoCash0.json';
 import casiNoCash1Json from '$lib/crc-decks/data-col/casiNoCash1.json';
 import teamSyncAuth0Json from '$lib/crc-decks/data-col/teamSyncAuth0.json';
 import teamSyncAuth1Json from '$lib/crc-decks/data-col/teamSyncAuth1.json';
@@ -110,6 +110,51 @@ const asCommit = (json: DeckJson, base: SimpleDeck): SimpleDeck => {
   return out;
 };
 
+export const exampleDecks: Record<string, SimpleDeck> = {
+  rpg: deckWithIds(rpgJson),
+  library: deckWithIds(libraryJson),
+  hospital: deckWithIds(hospitalJson)
+};
+
+export const dataCollectionDecks: Record<string, SimpleDeck> = {
+  mediTrack: deckWithIds(mediTrackJson),
+  ecoTrack: deckWithIds(ecoTrackJson),
+  libraryAI0: deckWithIds(libAI0Json),
+  libraryCE0: deckWithIds(libCE0Json),
+  killGod0: deckWithIds(techDebtKillGod0Json),
+  makeGod0: deckWithIds(techDebtMakeGod0Json),
+  casiNoAI0: deckWithIds(casiNoAI0Json),
+  casiNoWeb0: deckWithIds(casiNoWeb0Json),
+  casiNoCash0: deckWithIds(casiNoCash0Json),
+  teamSyncAuth0: deckWithIds(teamSyncAuth0Json)
+};
+
+dataCollectionDecks.libraryAI1 = asCommit(libAI1Json, dataCollectionDecks.libraryAI0);
+dataCollectionDecks.libraryCE1 = asCommit(libCE1Json, dataCollectionDecks.libraryCE0);
+dataCollectionDecks.killGod1 = asCommit(techDebtKillGod1Json, dataCollectionDecks.killGod0);
+dataCollectionDecks.makeGod1 = asCommit(techDebtMakeGod1Json, dataCollectionDecks.makeGod0);
+dataCollectionDecks.casiNoAI1 = asCommit(casiNoAI1Json, dataCollectionDecks.casiNoAI0);
+dataCollectionDecks.casiNoWeb1 = asCommit(casiNoWeb1Json, dataCollectionDecks.casiNoWeb0);
+dataCollectionDecks.casiNoCash1 = asCommit(casiNoCash1Json, dataCollectionDecks.casiNoCash0);
+dataCollectionDecks.teamSyncAuth1 = asCommit(teamSyncAuth1Json, dataCollectionDecks.teamSyncAuth0);
+
+dataCollectionDecks['a1-h0'] = dataCollectionDecks.libraryAI0;
+dataCollectionDecks['a1-h1'] = dataCollectionDecks.libraryAI1;
+dataCollectionDecks['a2-h0'] = dataCollectionDecks.libraryCE0;
+dataCollectionDecks['a2-h1'] = dataCollectionDecks.libraryCE1;
+dataCollectionDecks['b1-h0'] = dataCollectionDecks.killGod0;
+dataCollectionDecks['b1-h1'] = dataCollectionDecks.killGod1;
+dataCollectionDecks['b2-h0'] = dataCollectionDecks.makeGod0;
+dataCollectionDecks['b2-h1'] = dataCollectionDecks.makeGod1;
+dataCollectionDecks['c1-h0'] = dataCollectionDecks.casiNoCash0;
+dataCollectionDecks['c1-h1'] = dataCollectionDecks.casiNoCash1;
+dataCollectionDecks['c2-h0'] = dataCollectionDecks.casiNoAI0;
+dataCollectionDecks['c2-h1'] = dataCollectionDecks.casiNoAI1;
+dataCollectionDecks['c3-h0'] = dataCollectionDecks.casiNoWeb0;
+dataCollectionDecks['c3-h1'] = dataCollectionDecks.casiNoWeb1;
+dataCollectionDecks['d1-h0'] = dataCollectionDecks.teamSyncAuth0;
+dataCollectionDecks['d1-h1'] = dataCollectionDecks.teamSyncAuth1;
+
 const asAICommit = (json: DeckJson, base: SimpleDeck): SimpleDeck => {
   const out = json.cards as SimpleDeck;
   // todo: new cards sometimes don't have ids!?
@@ -117,127 +162,109 @@ const asAICommit = (json: DeckJson, base: SimpleDeck): SimpleDeck => {
   return out;
 };
 
-export const exampleDecks: Record<string, SimpleDeck> = {
-  rpg: deckWithIds(rpgJson),
-  library: deckWithIds(libraryJson),
-  hospital: deckWithIds(hospitalJson)
-};
+dataCollectionDecks['a1-z0'] = dataCollectionDecks['a1-h0'];
+dataCollectionDecks['a1-z1'] = asAICommit(a1cohereJson, dataCollectionDecks['a1-h0']);
+dataCollectionDecks['a2-z0'] = dataCollectionDecks['a2-h0'];
+dataCollectionDecks['a2-z1'] = asAICommit(a2cohereJson, dataCollectionDecks['a2-h0']);
+dataCollectionDecks['b1-z0'] = dataCollectionDecks['b1-h0'];
+dataCollectionDecks['b1-z1'] = asAICommit(b1cohereJson, dataCollectionDecks['b1-h0']);
+dataCollectionDecks['b2-z0'] = dataCollectionDecks['b2-h0'];
+dataCollectionDecks['b2-z1'] = asAICommit(b2cohereJson, dataCollectionDecks['b2-h0']);
+dataCollectionDecks['c1-z0'] = dataCollectionDecks['c1-h0'];
+dataCollectionDecks['c1-z1'] = asAICommit(c1cohereJson, dataCollectionDecks['c1-h0']);
+dataCollectionDecks['c2-z0'] = dataCollectionDecks['c2-h0'];
+dataCollectionDecks['c2-z1'] = asAICommit(c2cohereJson, dataCollectionDecks['c2-h0']);
+dataCollectionDecks['c3-z0'] = dataCollectionDecks['c3-h0'];
+dataCollectionDecks['c3-z1'] = asAICommit(c3cohereJson, dataCollectionDecks['c3-h0']);
+dataCollectionDecks['a1-y0'] = dataCollectionDecks['a1-h0'];
+dataCollectionDecks['a1-y1'] = asAICommit(a1openaiJson, dataCollectionDecks['a1-h0']);
+dataCollectionDecks['a2-y0'] = dataCollectionDecks['a2-h0'];
+dataCollectionDecks['a2-y1'] = asAICommit(a2openaiJson, dataCollectionDecks['a2-h0']);
+dataCollectionDecks['b1-y0'] = dataCollectionDecks['b1-h0'];
+dataCollectionDecks['b1-y1'] = asAICommit(b1openaiJson, dataCollectionDecks['b1-h0']);
+dataCollectionDecks['b2-y0'] = dataCollectionDecks['b2-h0'];
+dataCollectionDecks['b2-y1'] = asAICommit(b2openaiJson, dataCollectionDecks['b2-h0']);
+dataCollectionDecks['c1-y0'] = dataCollectionDecks['c1-h0'];
+dataCollectionDecks['c1-y1'] = asAICommit(c1openaiJson, dataCollectionDecks['c1-h0']);
+dataCollectionDecks['c2-y0'] = dataCollectionDecks['c2-h0'];
+dataCollectionDecks['c2-y1'] = asAICommit(c2openaiJson, dataCollectionDecks['c2-h0']);
+dataCollectionDecks['c3-y0'] = dataCollectionDecks['c3-h0'];
+dataCollectionDecks['c3-y1'] = asAICommit(c3openaiJson, dataCollectionDecks['c3-h0']);
+dataCollectionDecks['a1-x0'] = dataCollectionDecks['a1-h0'];
+dataCollectionDecks['a1-x1'] = asAICommit(a1sonnetJson, dataCollectionDecks['a1-h0']);
+dataCollectionDecks['a2-x0'] = dataCollectionDecks['a2-h0'];
+dataCollectionDecks['a2-x1'] = asAICommit(a2sonnetJson, dataCollectionDecks['a2-h0']);
+dataCollectionDecks['b1-x0'] = dataCollectionDecks['b1-h0'];
+dataCollectionDecks['b1-x1'] = asAICommit(b1sonnetJson, dataCollectionDecks['b1-h0']);
+dataCollectionDecks['b2-x0'] = dataCollectionDecks['b2-h0'];
+dataCollectionDecks['b2-x1'] = asAICommit(b2sonnetJson, dataCollectionDecks['b2-h0']);
+dataCollectionDecks['c1-x0'] = dataCollectionDecks['c1-h0'];
+dataCollectionDecks['c1-x1'] = asAICommit(c1sonnetJson, dataCollectionDecks['c1-h0']);
+dataCollectionDecks['c2-x0'] = dataCollectionDecks['c2-h0'];
+dataCollectionDecks['c2-x1'] = asAICommit(c2sonnetJson, dataCollectionDecks['c2-h0']);
+dataCollectionDecks['c3-x0'] = dataCollectionDecks['c3-h0'];
+dataCollectionDecks['c3-x1'] = asAICommit(c3sonnetJson, dataCollectionDecks['c3-h0']);
+dataCollectionDecks['a1-w0'] = dataCollectionDecks['a1-h0'];
+dataCollectionDecks['a1-w1'] = asAICommit(a1haikuJson, dataCollectionDecks['a1-h0']);
+dataCollectionDecks['a2-w0'] = dataCollectionDecks['a2-h0'];
+dataCollectionDecks['a2-w1'] = asAICommit(a2haikuJson, dataCollectionDecks['a2-h0']);
+dataCollectionDecks['b1-w0'] = dataCollectionDecks['b1-h0'];
+dataCollectionDecks['b1-w1'] = asAICommit(b1haikuJson, dataCollectionDecks['b1-h0']);
+dataCollectionDecks['b2-w0'] = dataCollectionDecks['b2-h0'];
+dataCollectionDecks['b2-w1'] = asAICommit(b2haikuJson, dataCollectionDecks['b2-h0']);
+dataCollectionDecks['c1-w0'] = dataCollectionDecks['c1-h0'];
+dataCollectionDecks['c1-w1'] = asAICommit(c1haikuJson, dataCollectionDecks['c1-h0']);
+dataCollectionDecks['c2-w0'] = dataCollectionDecks['c2-h0'];
+dataCollectionDecks['c2-w1'] = asAICommit(c2haikuJson, dataCollectionDecks['c2-h0']);
+dataCollectionDecks['c3-w0'] = dataCollectionDecks['c3-h0'];
+dataCollectionDecks['c3-w1'] = asAICommit(c3haikuJson, dataCollectionDecks['c3-h0']);
 
-type DeckCode =
-  /** library */
-  | 'a'
-  /** god */
-  | 'b'
-  /** casino */
-  | 'c';
+type DeckCode = 'a' | 'b' | 'c';
 export type ProblemCode = `${DeckCode}${1 | 2}` | 'c3';
 type AgentCode = 'h' | 'w' | 'x' | 'y' | 'z';
-type SolutionStub = { prob: ProblemCode; agent: AgentCode };
-deckWithIds(mediTrackJson);
-deckWithIds(ecoTrackJson);
-const problemStartDecks: Record<ProblemCode, SimpleDeck> = {
-  a1: deckWithIds(libAI0Json),
-  a2: deckWithIds(libCE0Json),
-  b1: deckWithIds(godKill0Json),
-  b2: deckWithIds(godMake0Json),
-  c1: deckWithIds(noAI0Json),
-  c2: deckWithIds(noWeb0Json),
-  c3: deckWithIds(noCash0Json)
-};
-deckWithIds(teamSyncAuth0Json);
-
-const agentSolutionDecks: Record<AgentCode, Record<ProblemCode, SimpleDeck>> = {
-  h: {
-    a1: asCommit(libAI1Json, problemStartDecks['a1']),
-    a2: asCommit(libCE1Json, problemStartDecks['a2']),
-    b1: asCommit(techDebtKillGod1Json, problemStartDecks['b1']),
-    b2: asCommit(techDebtMakeGod1Json, problemStartDecks['b2']),
-    c1: asCommit(casiNoAI1Json, problemStartDecks['c1']),
-    c2: asCommit(casiNoWeb1Json, problemStartDecks['c2']),
-    c3: asCommit(casiNoCash1Json, problemStartDecks['c3'])
-  },
-  z: {
-    a1: asAICommit(a1cohereJson, problemStartDecks['a1']),
-    a2: asAICommit(a2cohereJson, problemStartDecks['a2']),
-    b1: asAICommit(b1cohereJson, problemStartDecks['b1']),
-    b2: asAICommit(b2cohereJson, problemStartDecks['b2']),
-    c1: asAICommit(c1cohereJson, problemStartDecks['c1']),
-    c2: asAICommit(c2cohereJson, problemStartDecks['c2']),
-    c3: asAICommit(c3cohereJson, problemStartDecks['c3'])
-  },
-  y: {
-    a1: asAICommit(a1openaiJson, problemStartDecks['a1']),
-    a2: asAICommit(a2openaiJson, problemStartDecks['a2']),
-    b1: asAICommit(b1openaiJson, problemStartDecks['b1']),
-    b2: asAICommit(b2openaiJson, problemStartDecks['b2']),
-    c1: asAICommit(c1openaiJson, problemStartDecks['c1']),
-    c2: asAICommit(c2openaiJson, problemStartDecks['c2']),
-    c3: asAICommit(c3openaiJson, problemStartDecks['c3'])
-  },
-  x: {
-    a1: asAICommit(a1sonnetJson, problemStartDecks['a1']),
-    a2: asAICommit(a2sonnetJson, problemStartDecks['a2']),
-    b1: asAICommit(b1sonnetJson, problemStartDecks['b1']),
-    b2: asAICommit(b2sonnetJson, problemStartDecks['b2']),
-    c1: asAICommit(c1sonnetJson, problemStartDecks['c1']),
-    c2: asAICommit(c2sonnetJson, problemStartDecks['c2']),
-    c3: asAICommit(c3sonnetJson, problemStartDecks['c3'])
-  },
-  w: {
-    a1: asAICommit(a1haikuJson, problemStartDecks['a1']),
-    a2: asAICommit(a2haikuJson, problemStartDecks['a2']),
-    b1: asAICommit(b1haikuJson, problemStartDecks['b1']),
-    b2: asAICommit(b2haikuJson, problemStartDecks['b2']),
-    c1: asAICommit(c1haikuJson, problemStartDecks['c1']),
-    c2: asAICommit(c2haikuJson, problemStartDecks['c2']),
-    c3: asAICommit(c3haikuJson, problemStartDecks['c3'])
-  }
-};
-
-// dataCollectionDecks['d1-h1'] = asCommit(teamSyncAuth1Json, dataCollectionDecks.teamSyncAuth0);
+type SolutionStub = `${ProblemCode}-${AgentCode}`;
 
 const animalMap: Record<string, SolutionStub> = {
-  bear: { prob: 'b1', agent: 'w' },
-  camel: { prob: 'b1', agent: 'x' },
-  cheetah: { prob: 'b2', agent: 'h' },
-  deer: { prob: 'b1', agent: 'z' },
-  eagle: { prob: 'b1', agent: 'y' },
-  ferret: { prob: 'c1', agent: 'y' },
-  fox: { prob: 'c1', agent: 'w' },
-  gecko: { prob: 'a2', agent: 'x' },
-  goat: { prob: 'c3', agent: 'w' },
-  hare: { prob: 'a2', agent: 'w' },
-  hawk: { prob: 'a1', agent: 'y' },
-  iguana: { prob: 'c3', agent: 'y' },
-  jaguar: { prob: 'b2', agent: 'z' },
-  koala: { prob: 'a1', agent: 'x' },
-  lemur: { prob: 'c3', agent: 'x' },
-  mole: { prob: 'b2', agent: 'x' },
-  newt: { prob: 'b2', agent: 'y' },
-  octopus: { prob: 'b1', agent: 'h' },
-  otter: { prob: 'a2', agent: 'z' },
-  panda: { prob: 'c2', agent: 'z' },
-  quail: { prob: 'c1', agent: 'z' },
-  rabbit: { prob: 'a1', agent: 'w' },
-  rhino: { prob: 'c3', agent: 'h' },
-  seal: { prob: 'b2', agent: 'w' },
-  snake: { prob: 'c2', agent: 'x' },
-  squid: { prob: 'a1', agent: 'h' },
-  tiger: { prob: 'c3', agent: 'z' },
-  turtle: { prob: 'c1', agent: 'h' },
-  vole: { prob: 'a1', agent: 'z' },
-  vulture: { prob: 'a2', agent: 'h' },
-  weasel: { prob: 'a2', agent: 'y' },
-  xerus: { prob: 'c2', agent: 'y' },
-  yak: { prob: 'c1', agent: 'x' },
-  walrus: { prob: 'c2', agent: 'h' },
-  zebra: { prob: 'c2', agent: 'w' }
+  bear: 'b1-w',
+  camel: 'b1-x',
+  cheetah: 'b2-h',
+  deer: 'b1-z',
+  eagle: 'b1-y',
+  ferret: 'c1-y',
+  fox: 'c1-w',
+  gecko: 'a2-x',
+  goat: 'c3-w',
+  hare: 'a2-w',
+  hawk: 'a1-y',
+  iguana: 'c3-y',
+  jaguar: 'b2-z',
+  koala: 'a1-x',
+  lemur: 'c3-x',
+  mole: 'b2-x',
+  newt: 'b2-y',
+  octopus: 'b1-h',
+  otter: 'a2-z',
+  panda: 'c2-z',
+  quail: 'c1-z',
+  rabbit: 'a1-w',
+  rhino: 'c3-h',
+  seal: 'b2-w',
+  snake: 'c2-x',
+  squid: 'a1-h',
+  tiger: 'c3-z',
+  turtle: 'c1-h',
+  vole: 'a1-z',
+  vulture: 'a2-h',
+  weasel: 'a2-y',
+  xerus: 'c2-y',
+  yak: 'c1-x',
+  walrus: 'c2-h',
+  zebra: 'c2-w'
 };
 
-const dataCollectionDecks: Record<string, SimpleDeck> = {};
 for (const [animal, stub] of Object.entries(animalMap)) {
-  dataCollectionDecks[animal + '-0'] = problemStartDecks[stub.prob];
-  dataCollectionDecks[animal + '-1'] = agentSolutionDecks[stub.agent][stub.prob];
+  dataCollectionDecks[animal + '-0'] = dataCollectionDecks[stub + '0'];
+  dataCollectionDecks[animal + '-1'] = dataCollectionDecks[stub + '1'];
 }
 
 export const premadeDecks: Record<string, SimpleDeck> = {
